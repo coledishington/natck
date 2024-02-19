3d8de8c04e9664c7cd4ecee0edcc837158bd16b2
Cole Dishington <coledishington@gmail.com>
2024-02-20 11:14:37 +1300

    Check persistent connections are used in MeasureMaxConnections testing
    Persistent connections are needed to accurately measure the max number
    of connections a host can use.

--- main_test.go
+++ main_test.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"path"
+	"sync"
 	"testing"
 )
+
+type httpServerStats struct {
+	m           sync.Mutex
+	connections int
+}
 
 func tPath(p ...string) string {
 	p = append([]string{"testdata"}, p...)
@@ -48,7 +55,7 @@ func cpFile(t *testing.T, sPath, dPath string) {
 	}
 }
 
-func spawnHttpServer(t *testing.T, testdata string, port int) {
+func spawnHttpServer(t *testing.T, testdata string, port int) *httpServerStats {
 	dir := t.TempDir()
 	t.Cleanup(func() {
 		err := os.RemoveAll(dir)
@@ -58,9 +65,20 @@ func spawnHttpServer(t *testing.T, testdata string, port int) {
 	})
 
 	cpFile(t, testdata, path.Join(dir, "index.html"))
+
+	var stats httpServerStats
+	statsCb := func(c net.Conn, s http.ConnState) {
+		stats.m.Lock()
+		defer stats.m.Unlock()
+		if s == http.StateNew {
+			stats.connections++
+		}
+	}
+
 	srv := http.Server{
-		Addr:    fmt.Sprint(":", port),
-		Handler: http.FileServer(http.Dir(dir)),
+		Addr:      fmt.Sprint(":", port),
+		Handler:   http.FileServer(http.Dir(dir)),
+		ConnState: statsCb,
 	}
 	t.Cleanup(func() {
 		err := srv.Close()
@@ -75,6 +93,7 @@ func spawnHttpServer(t *testing.T, testdata string, port int) {
 			t.Errorf("Unexpected shutdown of http server on port %v: %v", port, err)
 		}
 	}()
+	return &stats
 }
 
 func TestMeasureMaxConnections(t *testing.T) {
@@ -104,12 +123,14 @@ func TestMeasureMaxConnections(t *testing.T) {
 		},
 	}
 
-	spawnHttpServer(t, tPath("no_links.html"), 8081)
-	spawnHttpServer(t, tPath("no_links.html"), 8082)
-	spawnHttpServer(t, tPath("no_links.html"), 8083)
-
 	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
+			httpStats := []*httpServerStats{
+				spawnHttpServer(t, tPath("no_links.html"), 8081),
+				spawnHttpServer(t, tPath("no_links.html"), 8082),
+				spawnHttpServer(t, tPath("no_links.html"), 8083),
+			}
+
 			urls := []url.URL{}
 			for _, inUrl := range tc.inUrls {
 				u, err := url.Parse(inUrl)
@@ -121,6 +142,13 @@ func TestMeasureMaxConnections(t *testing.T) {
 			nConns := MeasureMaxConnections(urls)
 			if nConns != tc.outNConns {
 				t.Error("expected (nConns=", tc.outNConns, "), got (nConns=", nConns, ")")
+			}
+			for i := range httpStats {
+				httpStats[i].m.Lock()
+				if httpStats[i].connections > 1 {
+					t.Error("expected no more than one connection per http server, got", httpStats[i].connections)
+				}
+				httpStats[i].m.Unlock()
 			}
 		})
 	}
