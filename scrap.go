1e5ef50294681c591774f6777549c45d427da9f3
Cole Dishington <coledishington@gmail.com>
2024-03-24 11:37:13 +1300

    Switch scraper to html parser from tokenizer
    Switch scraper to html parser from tokenizer to simplify the adddition
    of new html scraping.

--- scrap.go
+++ scrap.go
@@ -5,66 +5,84 @@ import (
 	"net/url"
 
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 func urlCmp(u1, u2 *url.URL) bool {
 	return u1.Host == u2.Host && u1.Path == u2.Path
 }
 
-func Scrap(host *url.URL, body io.Reader) []*url.URL {
-	urls := []*url.URL{}
-
-	for z := html.NewTokenizer(body); ; {
-		tt := z.Next()
-		if tt == html.ErrorToken {
+func findAllAtomTagInNode(n *html.Node, tag atom.Atom) []*html.Node {
+	matches := []*html.Node{}
+	stack := []*html.Node{}
+	for iter := n; iter != nil; {
+		if iter.DataAtom == tag {
+			matches = append(matches, iter)
+		}
+		for child := iter.FirstChild; child != nil; child = child.NextSibling {
+			stack = append(stack, child)
+		}
+		if len(stack) == 0 {
 			break
 		}
+		iter = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+	}
+	return matches
+}
 
-		if tt != html.StartTagToken {
-			continue
-		}
-
-		name, hasAttrs := z.TagName()
-		if string(name) != "a" {
-			continue
-		}
-
-		for more := hasAttrs; more; {
-			var k, v []byte
-
-			k, v, more = z.TagAttr()
-			aKey := string(k)
-			if string(aKey) != "href" {
-				continue
-			}
-
-			aVal := string(v)
-			aUrl, err := url.Parse(aVal)
-			if err != nil {
-				// Invalid url in href
-				continue
-			}
-
-			nUrl := aUrl
-			if !aUrl.IsAbs() {
-				// Handle relative links
-				path := aUrl.Path
-				*nUrl = *host
-				nUrl.Path = path
-			}
-
-			found := false
-			for _, u := range urls {
-				found = urlCmp(nUrl, u)
-				if found {
-					break
-				}
-			}
-			if found {
-				continue
-			}
-			urls = append(urls, nUrl)
+func findAtomAttrInNode(n *html.Node, needle atom.Atom) (html.Attribute, bool) {
+	for _, a := range n.Attr {
+		if atom.Lookup([]byte(a.Key)) == needle {
+			return a, true
 		}
 	}
+	return html.Attribute{}, false
+}
+
+func findHref(n *html.Node) (*url.URL, error) {
+	href, found := findAtomAttrInNode(n, atom.Href)
+	if !found {
+		return nil, nil
+	}
+
+	return url.Parse(href.Val)
+}
+
+func Scrap(host *url.URL, body io.Reader) []*url.URL {
+	urls := []*url.URL{}
+	doc, err := html.Parse(body)
+	if err != nil {
+		return urls
+	}
+
+	// Parse gettable urls
+	links := findAllAtomTagInNode(doc, atom.A)
+	for _, n := range links {
+		u, err := findHref(n)
+		if u == nil || err != nil {
+			continue
+		}
+
+		nUrl := u
+		if !nUrl.IsAbs() {
+			path := nUrl.Path
+			*nUrl = *host
+			nUrl.Path = path
+		}
+
+		found := false
+		for _, u := range urls {
+			found = urlCmp(nUrl, u)
+			if found {
+				break
+			}
+		}
+		if found {
+			continue
+		}
+		urls = append(urls, nUrl)
+	}
+
 	return urls
 }
